a4cdf9f594eba08f110b4412ca4846f8c4baab99
Freddie <freddieburns@hotmail.co.uk>
2020-10-11 22:25:05 +0100

    Updated ALLOWED_HOSTS
    This is to allow debugging using the djangogirls host later.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zv--9-*&&a&i5(q8*h!e2f1c-mf@0$o(a$mrpt4j#0e=e5bdzi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
