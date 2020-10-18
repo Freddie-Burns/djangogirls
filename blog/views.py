cbce132649c1175ab96886fd885f9d064b91492b
Freddie <freddieburns@hotmail.co.uk>
2020-10-18 15:41:35 +0100

    Redirect to new post when created

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 
 from .forms import PostForm
@@ -23,6 +23,7 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
